b7cf31d9d0eb4e75434d20d2f883319a57a8cc3f
Remnant <Remnantnew223@gmail.com>
2024-08-06 09:42:56 +0530

    Login Registration New Architecture - WIP

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -2,8 +2,8 @@ export default function LoginPage() {
     return (
         <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                {/* <img className="mx-auto h-10 w-auto"
-                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/> */}
+                <img className="mx-auto h-10 w-auto"
+                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/>
                 <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to
                     your account</h2>
             </div>
@@ -97,7 +97,7 @@ export default function LoginPage() {
                 </div>
 
                 <p className="mt-10 text-center text-sm text-gray-500 px-3">
-                        Not member?     
+                        Not member?
                         <a href="/registration"
                                 className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign Up
                         </a>
--- src/components/Auth/RegisterForm.tsx
+++ src/components/Auth/RegisterForm.tsx
@@ -1,17 +1,81 @@
+import { useState } from "react";
+import {useRouter} from "next/router";
+
+
 export default function Registration() {
+    const router = useRouter();
+    const [isUpdating, setIsUpdating] = useState(false);
+    const id = router.query.id;
+    const [showForm, setShowForm] = useState(true);
+    const [rePassword, setRePassword] = useState('');
+
+    const [formData, setFormData] = useState({
+        FirstName: '',
+        LastName: '',
+        Address: '',
+        Email: '',
+        PhoneNumber: '',
+        Country   : '',
+        PostalCode: '',
+        Province: '',
+        City: '',
+        Password: '',
+    });
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('Form Data:', formData);
+        try {
+
+            const method = isUpdating ? 'PUT' : 'POST';
+            const url = isUpdating ? `http://localhost:5025/api/Users/${id}` : 'http://localhost:5025/api/Users';
+            const response = await fetch(url, {
+                method,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                console.log(`Order ${isUpdating ? 'updated' : 'created'} successfully!`);
+                // Optionally: Reset the form after successful submission
+                setFormData({
+                    FirstName: '',
+                    LastName: '',
+                    Address: '',
+                    Email: '',
+                    PhoneNumber: '',
+                    Country   : '',
+                    PostalCode: '',
+                    Province: '',
+                    City: '',
+                    Password: '',
+                });
+                setShowForm(false);
+            } else {
+                console.error(`Failed to ${isUpdating ? 'update' : 'create'} order:`, await response.text());
+                // Handle error scenario: show error message to user
+            }
+        } catch (error) {
+            console.error('Error ${isUpdating ? \'updating\' : \'creating\'} order:', error);
+            // Handle error scenario: show error message to user
+        }
+    };
+    
     return (
         <div className="isolate bg-white px-6 py-5 sm:py-5 lg:px-8 md:w-full rounded-lg mx-3">
             <div className="mx-auto text-left">
                 <h2 className="text-lg tracking-tight text-gray-900 sm:text-4xl">Registration Form</h2>
             </div>
-            <form action="#" method="POST" className="mt-4 w-full ">
+            <form action="#" onSubmit={handleSubmit} method="POST" className="mt-4 w-full ">
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
 
                     <div>
                         <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">First
                             name</label>
                         <div className="mt-2.5">
-                            <input type="text" name="first-name" id="first-name" autoComplete="given-name"
+                            <input type="text" name="FirstName" id="first-name" autoComplete="given-name"
                                    className="block w-full  border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
@@ -19,7 +83,7 @@ export default function Registration() {
                         <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">Last
                             name</label>
                         <div className="mt-2.5">
-                            <input type="text" name="last-name" id="last-name" autoComplete="family-name"
+                            <input type="text" name="LastName" id="last-name" autoComplete="family-name"
                                    className="block w-full  border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
@@ -27,7 +91,7 @@ export default function Registration() {
                         <label htmlFor="first-name"
                                className="block text-sm font-semibold leading-6 text-gray-900">Email</label>
                         <div className="mt-2.5">
-                            <input type="text" name="first-name" id="first-name" autoComplete="given-name"
+                            <input type="text" name="Email" id="first-name" autoComplete="given-name"
                                    className="block w-full  border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
@@ -37,7 +101,7 @@ export default function Registration() {
                         <div className="relative mt-2.5">
                             <div className="absolute inset-y-0 left-0 flex items-center">
                                 <label htmlFor="country" className="sr-only">Country</label>
-                                <select id="country" name="country"
+                                <select id="country" name="PhoneNumber"
                                         className="h-full  border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
                                     <option>US</option>
                                     <option>CA</option>
@@ -54,21 +118,24 @@ export default function Registration() {
                                    className="block w-full  border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
+
+
                     <div className="sm:col-span-2">
                         <label htmlFor="message"
-                               className="block text-sm font-semibold leading-6 text-gray-900">Message</label>
+                               className="block text-sm font-semibold leading-6 text-gray-900">Address</label>
                         <div className="mt-2.5">
-                            <textarea name="message" id="message"
+                            <textarea name="Address" id="message"
                                       className="block w-full h-[40px] border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                         </div>
                     </div>
+
                     <div className="sm:col-span-2">
                         <label htmlFor="company"
                                className="block text-sm font-semibold leading-6 text-gray-900">Country Session</label>
                         <div className="mt-2.5">
                             <label htmlFor="company"
                                    className="block text-sm leading-6 text-gray-900">Country</label>
-                            <input type="text" name="company" id="company" autoComplete="organization"
+                            <input type="text" name="Country" id="company" autoComplete="organization"
                                    className="block w-full  border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                         </div>
                         <div className=" grid grid-cols-1 md:grid-cols-2 gap-3 mt-3">
@@ -76,7 +143,7 @@ export default function Registration() {
                                 <label htmlFor="company"
                                        className="block text-sm leading-6 text-gray-900">Province</label>
                                 <div className="mt-2.5">
-                                    <input type="text" name="company" id="company" autoComplete="organization"
+                                    <input type="text" name="Province" id="company" autoComplete="organization"
                                            className="block w-full  border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                                 </div>
                             </div>
@@ -84,7 +151,7 @@ export default function Registration() {
                                 <label htmlFor="company"
                                        className="block text-sm leading-6 text-gray-900">City</label>
                                 <div className="mt-2.5">
-                                    <input type="text" name="company" id="company" autoComplete="organization"
+                                    <input type="text" name="City" id="company" autoComplete="organization"
                                            className="block w-full  border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                                 </div>
                             </div>
@@ -92,20 +159,20 @@ export default function Registration() {
                                 <label htmlFor="company"
                                        className="block text-sm leading-6 text-gray-900">Postal</label>
                                 <div className="mt-2.5">
-                                    <input type="text" name="company" id="company" autoComplete="organization"
+                                    <input type="text" name="PostalCode" id="company" autoComplete="organization"
                                            className="block w-full  border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                                 </div>
                             </div>
-                            <div>
-                                <div className="sm:col-span-2">
-                                    <label htmlFor="message"
-                                           className="block text-sm font-semibold leading-6 text-gray-900">Message</label>
-                                    <div className="mt-2.5">
-                            <textarea name="message" id="message"
-                                      className="block w-full h-[40px] border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
-                                    </div>
-                                </div>
-                            </div>
+                            {/*<div>*/}
+                            {/*    <div className="sm:col-span-2">*/}
+                            {/*        <label htmlFor="message"*/}
+                            {/*               className="block text-sm font-semibold leading-6 text-gray-900">Message</label>*/}
+                            {/*        <div className="mt-2.5">*/}
+                            {/*<textarea name="message" id="message"*/}
+                            {/*          className="block w-full h-[40px] border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>*/}
+                            {/*        </div>*/}
+                            {/*    </div>*/}
+                            {/*</div>*/}
                         </div>
                     </div>
                     <div className="sm:col-span-2">
@@ -114,7 +181,7 @@ export default function Registration() {
                         <div className="mt-2.5">
                             <label htmlFor="email"
                                    className="block text-sm leading-6 text-gray-900">Password</label>
-                            <input type="password" name="password" id="email" autoComplete="email"
+                            <input type="password" name="Password" id="email" autoComplete="email"
                                    className="block w-full  border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
@@ -122,10 +189,11 @@ export default function Registration() {
                         <label htmlFor="email"
                                className="block text-sm leading-6 text-gray-900">Re-Enter Password</label>
                         <div className="mt-2.5">
-                            <input type="password" name="password" id="email" autoComplete="email"
+                            <input type="password" name="Re-Password" id="email" autoComplete="email"
                                    className="block w-full  border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
+
                     <div className="flex gap-x-4 sm:col-span-2">
                         <div className="flex h-6 items-center">
                             <button type="button"
