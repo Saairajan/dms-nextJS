e7dde581cb6a0e8c299aa05a9c817acc1a5ec669
L.Saai <137699784+Remnant12@users.noreply.github.com>
2024-08-07 13:34:56 +0530

    new env file created

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 
-const API_BASE_URL = 'http://localhost:5025/api';
+// const API_BASE_URL = 'http://localhost:5025/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5025/api';
 
 // Function to handle user registration
 export const registerUser = async (userData: {
